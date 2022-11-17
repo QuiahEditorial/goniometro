199895bcd59a2b5206a06bee51364eba6b6aff7a
Roberto A Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-17 13:29:46 -0600

    Update goniometro.ino

--- goniometro.ino
+++ goniometro.ino
@@ -1,13 +1,14 @@
-// https://es.overleaf.com/read/bxyszzbykrqx
-// DNv7acPAeVBhTXbKv26itJecPG1SPy2o4F
+// Por Roberto A. Zavala
+// Breve : https://es.overleaf.com/read/bxyszzbykrqx
+// 🙏🏼    : DNv7acPAeVBhTXbKv26itJecPG1SPy2o4F
 
 float m = 0.31578;         // 950 bit / 300°
 float b = -150;            // La lectura será de 0° cuando la perilla esté justo a la mitad
 float V;                   // El voltaje en bits
-
+float a;                   // El ángulo en grados
 void setup()
 {
-  Serial.begin(9600)
+  Serial.begin(9600);
 }
 
 void loop()
