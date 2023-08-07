9dac783e049163b68e15ee59afdbe0c05ac61e2b
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2023-08-07 15:44:46 -0600

    Update goniometro.ino

--- goniometro.ino
+++ goniometro.ino
@@ -1,7 +1,10 @@
 // Por Roberto A. Zavala
+// QuiahEditorial R
 // Breve : https://es.overleaf.com/read/bxyszzbykrqx
 // Libro : https://www.amazon.com.mx/dp/B074TTGLL2
-// 🙏🏼    : DNv7acPAeVBhTXbKv26itJecPG1SPy2o4F
+// Tienda:     https://www.facebook.com/LaTeXnomicon/shop_tab
+// X :         @EditorialQuiah
+// 🙏🏼 DogeTip: DNv7acPAeVBhTXbKv26itJecPG1SPy2o4F
 
 float m = 0.31578;         // 950 bit / 300°
 float b = -150;            // La lectura será de 0° cuando la perilla esté justo a la mitad
